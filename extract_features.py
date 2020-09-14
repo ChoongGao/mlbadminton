1c8c3f9e9a97967411115b94f7b0590a5923d656
ChoongGao <choonggao.lee@utexas.edu>
2020-09-13 22:35:48 -0500

    commented and cleaned up files

--- extract_features.py
+++ extract_features.py
@@ -7,16 +7,23 @@ import numpy as np
 
 # Constants
 PATH_TO_RAW_CSV = './raw_csv' # Path to raw csv data
+PATH_TO_EXTRACTED = './extracted_data/initial_training.csv' # Path where extracted features will be saved
 SWING_TYPES = ['smash', 'drop', 'clear'] # List of the possible swing types
-SWING_TO_INT = {'smash': 0, 'drop': 1, 'clear': 2}
-AXES = ['x', 'y', 'z']
-NUM_END = 1
+SWING_TO_INT = {'smash': 0, 'drop': 1, 'clear': 2} # Mapping of swing type to integer representation
+AXES = ['x', 'y', 'z'] # Three axes that measurements come in
+NUM_END = 1 # Case where final data entry is incomplete
+TRAINING_HEADERS = ['Swing Type', 'MinWX', 'MaxWX', 'MinWY', 'MaxWY', 'MinWZ', 'MaxWZ', 'MinAX', 'MaxAX', 'MinAY', 'MaxAY', 'MinAZ', 'MaxAZ'] # Headers for the training dataframe
+FILE_NAME = 'initial_training.csv' # Name training data will be saved as
 
-# Function that will take input of the swing dataframe, will output numpy array of extracted features
+
+# Takes input of the swing dataframe, outputs array of label extracted features
 def extract_features(df, label):
+    # Case where final data entry is incomplete
     df = df.drop(df.tail(NUM_END).index)
+    # Create array and insert label
     extracted = []
     extracted.append(SWING_TO_INT[label])
+    # For angular velocity, find the maximum/minimum for all three directions and append
     for direction in AXES:
         col = 'w{}(deg/s)'.format(direction)
         data = df[[col]]
@@ -26,6 +33,7 @@ def extract_features(df, label):
         extracted.append(minimum)
         extracted.append(maximum)
 
+    # For acceleration, find the maximum/minimum for all three directions and append
     for direction in AXES:
         col = 'a{}(g)'.format(direction)
         data = df[[col]]
@@ -34,6 +42,7 @@ def extract_features(df, label):
         maximum = arr.max()
         extracted.append(minimum)
         extracted.append(maximum)
+
     return extracted
 
 
@@ -46,19 +55,19 @@ if __name__ == '__main__':
     sampled_swings = {}
     for swing_type in SWING_TYPES:
         swing_list = []
-        # Adds every swing of the swing type into a list
+        # Adds the path to every swing of the swing type into a list
         for root,dir,files in os.walk(os.path.join(PATH_TO_RAW_CSV, swing_type)):
             for name in files:
                 swing_list.append(os.path.join(root, name))
         
-        # For every swing of this type
+        # For every swing of this type, extract the features and append to the training list
         for swing_path in swing_list:
             df = pd.read_csv(swing_path)
             extracted_swing = extract_features(df, swing_type)
             training_list.append(extracted_swing)
     
-    # Move all the training data from the list to the dataframe
-    training_df = pd.DataFrame(data=training_list, columns=['Swing Type', 'MinWX', 'MaxWX', 'MinWY', 'MaxWY', 'MinWZ', 'MaxWZ', 'MinAX', 'MaxAX', 'MinAY', 'MaxAY', 'MinAZ', 'MaxAZ'])
-    saved_file = open('./extracted_data/initial_training.csv', 'w')
+    # Transfer training data from the list to the dataframe, then save to the folder
+    training_df = pd.DataFrame(data=training_list, columns=TRAINING_HEADERS)
+    saved_file = open(os.path.join(PATH_TO_EXTRACTED, FILE_NAME), 'w')
     training_df.to_csv(path_or_buf=saved_file)
     saved_file.close()
--- train_models.py
+++ train_models.py
@@ -7,13 +7,11 @@ import numpy as np
 from sklearn import model_selection, linear_model, neighbors
 
 # Constants
-PATH_TO_TRAINING_DIRECTORY = './extracted_data' # Path to raw csv data
-DATA_NAME = 'initial_training.csv'
+PATH_TO_TRAINING_DIRECTORY = './extracted_data' # Path to training data
+DATA_NAME = 'initial_training.csv' # Name of training data to access
 SWING_TYPES = ['smash', 'drop', 'clear'] # List of the possible swing types
-SWING_TO_INT = {'smash': 0, 'drop': 1, 'clear': 2}
-AXES = ['x', 'y', 'z']
-NUM_END = 1
-
+SWING_TO_INT = {'smash': 0, 'drop': 1, 'clear': 2} # Mapping of swing types to integer representation
+NUM_SPLITS = 5 # Number of splits/folds for model training
 
 if __name__ == '__main__':
 
@@ -25,15 +23,30 @@ if __name__ == '__main__':
     y = matrix[:,0]
     X = matrix[:,1:]
 
-    k_fold = model_selection.KFold(shuffle=True)
+    # Train and score models using cross validation
+    k_fold = model_selection.KFold(n_splits=NUM_SPLITS, shuffle=True)
+    # Will track scoring of different models
     logistic_scores = 0
+    neighbor_scores = 0
     for train_index, test_index in k_fold.split(X):
+        # Splits into training and testing data
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
-        # log_model = linear_model.LogisticRegression()
-        # log_model.fit(X_train, y_train)
+        # Training logistic regression model
+        log_model = linear_model.LogisticRegression()
+        log_model.fit(X_train, y_train)
+        logistic_scores += log_model.score(X_test, y_test)
 
+        # Training k nearest neighbor model
         nei_model = neighbors.KNeighborsClassifier(n_neighbors=10)
         nei_model.fit(X_train, y_train)
-        print(nei_model.score(X_test, y_test))
+        neighbor_scores += nei_model.score(X_test, y_test)
+
+    # Calculates average score of models
+    avg_log_score = logistic_scores/NUM_SPLITS
+    avg_nei_score = neighbor_scores/NUM_SPLITS
+
+    # Display results
+    print('Average success rate of logistic regression model: {}%'.format(avg_log_score*100))
+    print('Average success rate of k nearest neighbors model: {}%'.format(avg_nei_score*100))
